2e3425f9446103c3b4b9962843878a740596913b
tengfei,liang <44048948+tyutltf@users.noreply.github.com>
2018-12-20 19:55:00 +0800

    初始化项目

---
+++ blog/views.py
@@ -0,0 +1,96 @@
+from django.shortcuts import get_object_or_404,render
+from django.core.paginator import Paginator
+from .models import Blog,BlogType
+from django.conf import settings
+from django.db.models import Count
+from django.contrib.contenttypes.models import ContentType
+from read_statistics.utils import read_statistics_once_read
+from comment.models import Comment
+from comment.forms import CommentForm
+
+
+
+each_page_blogs_number=5
+
+def get_blog_list_common_data(request,blogs_all_list):
+    paginator = Paginator(blogs_all_list, each_page_blogs_number)  # 每5个博客为一页
+    page_num = request.GET.get('page', 1)  # 获取页码
+    page_of_blogs = paginator.get_page(page_num)
+    currentr_page_num = page_of_blogs.number  # 获取当前页码
+    # 获取当前页码 前后两页 并且排除 前两页
+    page_range = list(range(max(currentr_page_num - 2, 1), currentr_page_num)) + \
+                 list(range(currentr_page_num, min(currentr_page_num + 2, paginator.num_pages) + 1))
+    # 加上省略标记
+    if page_range[0] - 1 > 2:
+        page_range.insert(0, '...')
+    if paginator.num_pages - page_range[-1] >= 2:
+        page_range.append('...')
+
+    # 加上首页和尾页
+    if page_range[0] != 1:
+        page_range.insert(0, 1, )
+    if page_range[-1] != paginator.num_pages:
+        page_range.append(paginator.num_pages)
+
+    #获取博客分类对应的博客数量
+    '''
+    blog_types=BlogType.objects.all()
+    blog_types_list=[]
+    for blog_type in blog_types:
+        blog_type.blog_count=Blog.objects.filter(blog_type=blog_type).count()
+        blog_types_list.append(blog_type)
+    '''
+    #获取日期归档对应的博客数量
+    blog_dates=Blog.objects.dates('created_time','month',order="DESC")
+    blog_dates_dict={}
+    for blog_date in blog_dates:
+        blog_count=Blog.objects.filter(created_time__year=blog_date.year,created_time__month=blog_date.month).count()
+        blog_dates_dict[blog_date]=blog_count
+
+
+    context = {}
+    context['blogs'] = page_of_blogs.object_list
+    context['page_of_blogs'] = page_of_blogs
+    context['page_range'] = page_range
+    #context['blog_types'] = blog_types_list
+    context['blog_types'] = BlogType.objects.annotate(blog_count=Count('blog'))
+    context['blog_dates'] = blog_dates_dict
+    return context
+
+def blog_list(request):
+    blogs_all_list=Blog.objects.all()
+    context=get_blog_list_common_data(request,blogs_all_list)
+    return render(request,'blog/blog_list.html', context)
+
+def blog_detail(request,blog_pk):
+    blog = get_object_or_404(Blog, pk=blog_pk)
+    read_cookie_key=read_statistics_once_read(request,blog)
+    blog_content_type=ContentType.objects.get_for_model(blog)
+    comments=Comment.objects.filter(content_type=blog_content_type,object_id=blog.pk)
+
+    context={}
+    context['previous_blog']=Blog.objects.filter(created_time__gt=blog.created_time).last()  #最后一条为上一条博客
+    context['next_blog']=Blog.objects.filter(created_time__lt=blog.created_time).first()     #第一条为上一条
+    context['blog'] =blog
+    context['comments']=comments
+    data={}
+    data['content_type']=blog_content_type.model
+    data['object_id']=blog_pk
+    context['comment_form']=CommentForm(initial=data)
+    response= render(request,'blog/blog_detail.html', context)
+    response.set_cookie(read_cookie_key,'true')  #60s算一次阅读
+    return response
+
+def blogs_with_type(request,blog_type_pk):
+    blog_type=get_object_or_404(BlogType,pk=blog_type_pk)
+    blogs_all_list = Blog.objects.filter(blog_type=blog_type)
+
+    context = get_blog_list_common_data(request,blogs_all_list)
+    context['blog_type']=blog_type
+    return render(request,'blog/blog_with_type.html', context)
+
+def blogs_with_date(request,year,month):
+    blogs_all_list=Blog.objects.filter(created_time__year=year,created_time__month=month)
+    context = get_blog_list_common_data(request,blogs_all_list)
+    context['blogs_with_date']='%s年%s月'%(year,month)
+    return render(request,'blog/blog_with_date.html', context)
